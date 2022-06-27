b22bdd4465a0d091fda0cb0c9a79a3aed35b5098
Samuel Barbosa <contato@smkbarbosa.eti.br>
2022-06-27 13:55:47 -0300

    ex 25a

--- backend/src/portal_uft/src/portal_uft/subscribers/campus.py
+++ backend/src/portal_uft/src/portal_uft/subscribers/campus.py
@@ -1,4 +1,5 @@
 from kitconcept import api
+from plone import api as plone_api
 from portal_uft.content.campus import Campus
 from zope.lifecycleevent import ObjectAddedEvent
 from zope.lifecycleevent import ObjectModifiedEvent
@@ -14,10 +15,16 @@ def _update_tags(obj: Campus):
     obj.subject = tuple(tags)
 
 
+def _add_group(obj: Campus):
+    """Add group when the Campus is created"""
+    group = plone_api.group.create(groupname=obj.subject[0])
+
+
 def added(obj: Campus, event: ObjectAddedEvent):
     """A new Campus was added to the site"""
     # Atualiza tags do objeto
     _update_tags(obj)
+    _add_group(obj)
 
 
 def modified(obj: Campus, event: ObjectModifiedEvent):
