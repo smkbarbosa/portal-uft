6a01b1537500086ef5bd38e20136898da5127a74
Samuel Barbosa <contato@smkbarbosa.eti.br>
2022-06-29 15:10:55 -0300

    tentativa ex 43

--- popula_portal.py
+++ popula_portal.py
@@ -2,6 +2,7 @@ import json
 import requests
 import sys
 
+
 base_url = 'http://localhost:8080/Plone/++api++'
 username = 'admin'
 password = 'admin'
@@ -11,14 +12,43 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-# Authenticate
-response = requests.post(f'{base_url}/@login', headers=headers,
-                         json={'login': username, 'password': password})
 
-if response.status_code == 200:
-    token = response.json()['token']
-    headers['Authorization'] = f'Bearer {token}'
-    print(f'Autenticado! Token: {token}')
-else:
-    raise ValueError("Usuário ou senha inválidos")
+def auth():
+    # Authenticate
+    response = requests.post(f'{base_url}/@login', headers=headers,
+                             json={'login': username, 'password': password})
 
+    if response.status_code == 200:
+        token = response.json()['token']
+        headers['Authorization'] = f'Bearer {token}'
+        print(f'Autenticado! Token: {token}')
+    else:
+        raise ValueError("Usuário ou senha inválidos")
+
+
+def create_doc():
+    create = requests.post(f'{base_url}/Plone/post', headers=headers,
+                           json={
+                                '@type': 'Document',
+                               'id': 'gurupi',
+                               'title': 'Gurupi',
+                               'description': 'Cria campus via API'
+                           }, auth=(username, password))
+
+    publish = requests.post(f'{base_url}/Plone/gurupi/@workflow/publish', headers=headers,
+                            auth=(username, password))
+
+    if create.status_code == 200:
+        token = create.json()['token']
+        print(f'Conteúdo criado com sucesso!')
+    else:
+        raise ValueError('Erro ao criar conteúdo')
+
+    if publish.status_code == 200:
+        print(f'Conteúdo publicado com sucesso')
+    else:
+        raise ValueError('Erro ao publicar conteúdo')
+
+
+if __name__ == '__main__':
+    create_doc()
